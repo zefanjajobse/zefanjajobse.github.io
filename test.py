778ffef5137a2938cfba427846574d79e0d85402
zefanjajobse <zefanjaj@gmail.com>
2020-02-05 09:03:44 +0100

    testing

---
+++ test.py
@@ -0,0 +1,18 @@
+# Javascript
+#let number = 10;
+#if ((number % 2) == 0) {
+#    console.log("This number is even")
+#} else if ((number % 2) == 1) {
+#    console.log("This number isn't even")
+#} else (
+#    console.log("This is invalid")
+#)
+
+# python
+number = 10
+if (number % 2) == 0:
+    print("This number is even")
+elif (number % 2) == 1:
+    print("This number isn't even") 
+else:
+    print("This is invalid")
