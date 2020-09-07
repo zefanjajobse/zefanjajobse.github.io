0aa5a781f46db83d45d9482de443a7c9cea4bebd
Zefanja Jobse <zefanjaj@gmail.com>
2020-09-07 14:26:17 +0200

    code update

--- resources/colors.js
+++ resources/colors.js
@@ -1,14 +1,19 @@
 let listItems = Array.from(document.querySelectorAll(".list_item"));
 listItems.forEach(element => {
     let paragraphs = element.querySelectorAll("p");
-    let result = paragraphs[1].textContent.substring(9,99);
-    if (result == "nog te doen") {
+    let result = paragraphs[1].textContent.substring(9,99).split(/,/);
+    result.forEach(e => {
+        if (e == "nog te doen") {
         
-    } else if (result == "gestart") {
-        element.classList.add("started")
-    } else if (parseFloat(result) >= 5.5) {
-        element.classList.add("completed")
-    } else {
-        element.classList.add("redo")
-    }
+        } else if (e == "-") {
+            element.classList.add("started_important")
+        } else if (e == "gestart") {
+            element.classList.add("started")
+        } else if (parseFloat(e) < 5.5) {
+            element.classList.add("redo")
+        } else {
+            element.classList.add("completed")
+        }
+    });
+
 });
