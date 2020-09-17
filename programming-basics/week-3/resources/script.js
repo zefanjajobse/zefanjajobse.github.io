a8787cf53058b9d8fdf63574ecff8df3f9239ee0
Zefanja Jobse <zjobse@vmi361808.contaboserver.net>
2020-09-17 13:59:58 +0200

    week 3 voorbereiding

---
+++ programming-basics/week-3/resources/script.js
@@ -0,0 +1,16 @@
+let a = "value1";
+
+// switch statement
+
+switch (a) {
+    case "value1":
+        console.log(a)
+        break;
+
+    case "value2":
+        console.log(a)
+        break;
+
+    default:
+        break;
+}
